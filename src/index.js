f6affa719fe2e7b6ddbc0549463378eb395d52eb
FarAK10 <botirovfarhod115@gmail.com>
2022-01-05 19:36:51 +0500

    feat:modify index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    n = Math.abs(n);
+    var str = n.toString();
+    var end = 0;
+    for (let i = 0; i < str.length; i++) {
+        var cur = n % 10;
+        n = Math.floor(n / 10);
+        end = end * 10 + cur;
+    }
+    return end;
+};
